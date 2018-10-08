ee7b58b3d144249e11c00f544802f1d5d376b914
htcr <congruihetang@gmail.com>
2018-10-07 23:04:05 -0400

    tested drawing rectangle

--- gl-test.py
+++ gl-test.py
@@ -5,11 +5,29 @@ from OpenGL.GLU import *
 window = 0
 width, height = 500, 400
 
+def draw_rect(x, y, w, h):
+    glColor3f(0, 0, 1)
+    glBegin(GL_QUADS)
+    glVertex2f(x, y)
+    glVertex2f(x+w, y)
+    glVertex2f(x+w, y+h)
+    glVertex2f(x, y+h)
+    glEnd()
+
+def refresh2d(w, h):
+    glViewport(0, 0, w, h)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, w, 0, h, 0, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
 def draw():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-    
-    #TODO: draw rect
+    refresh2d(400, 500)
+
+    draw_rect(10, 20, 200, 300)
 
     glutSwapBuffers()
 
