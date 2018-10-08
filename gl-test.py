07846624e533f6e8fffe232292d04303d561c0c4
htcr <congruihetang@gmail.com>
2018-10-07 22:52:43 -0400

    able to draw an empty window

--- gl-test.py
+++ gl-test.py
@@ -2,4 +2,22 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-print('import ok!')
+window = 0
+width, height = 500, 400
+
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    
+    #TODO: draw rect
+
+    glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+glutInitWindowSize(width, height)
+glutInitWindowPosition(0, 0)
+window = glutCreateWindow('test-window')
+glutDisplayFunc(draw)
+glutIdleFunc(draw)
+glutMainLoop()
